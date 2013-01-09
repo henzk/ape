7bafd5340373d8a33011cf7d0d462b25ff3d6154
Hendrik Speidel <speidel@schnapptack.de>
2013-01-09 11:21:55 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+DEPS = ['featuremonkey']
+try:
+    import importlib
+except ImportError:
+    DEPS += ['importlib']
+
+try:
+    from setuptools import setup
+    extra = {
+        'install_requires' : DEPS
+    }
+except ImportError:
+    from distutils.core import setup
+    extra = {
+        'dependencies' : DEPS
+    }
+
+def read(fname):
+    import os.path
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='ape',
+    version='0.1',
+    description='A Productive Environment - make/rake/ant/fab-like system with support for FOSD',
+    long_description=read('README.rst'),
+    url='http://github.com/henzk/ape',
+    author='Hendrik Speidel',
+    author_email='hendrik@schnapptack.de',
+    license="MIT License",
+    keywords='fosd, fop, features, build tool',
+    packages=['ape'],
+    package_dir={'ape': 'ape'},
+    package_data={'ape': []},
+    include_package_data=True,
+    scripts=[],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Console',
+    ],
+    dependency_links = [
+        'http://github.com/henzk/featuremonkey/tarball/master#egg=featuremonkey-0.1',
+    ],
+    **extra
+)
